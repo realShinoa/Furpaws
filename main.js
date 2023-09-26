4082d729c4b9c5b391fbf2d5a759a2a26851a58e
realShinoa <scndella1@gmail.com>
2023-09-26 17:52:56 -0600

    added login/signup page
    ctto to kc, s'ya gumawa :^

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 sr.reveal ('.mapbox', {delay:300});
+
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let currentIndex = 0;
+
+function showSlide(index) {
+    images.forEach((image, i) => {
+        image.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
